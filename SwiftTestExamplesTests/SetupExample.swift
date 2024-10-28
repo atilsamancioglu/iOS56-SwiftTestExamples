ce3efd848c66d8c41a4c9a94fedb5abec30df6c3
Atil Samancioglu <samancioglu.atil@gmail.com>
2024-10-28 20:05:19 +0300

    new examples

---
+++ SwiftTestExamplesTests/SetupExample.swift
@@ -0,0 +1,16 @@
+//
+//  SetupTearDownExample.swift
+//  SwiftTestExamplesTests
+//
+//  Created by Atil Samancioglu on 28.10.2024.
+//
+
+import Testing
+
+struct SetupTearDownExample {
+
+    @Test func <#test function name#>() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ SwiftTestExamplesTests/SuiteExamples.swift
@@ -0,0 +1,16 @@
+//
+//  SuiteExamples.swift
+//  SwiftTestExamplesTests
+//
+//  Created by Atil Samancioglu on 28.10.2024.
+//
+
+import Testing
+
+struct SuiteExamples {
+
+    @Test func <#test function name#>() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
