8fec744c41a9fe3a446fdb1d1e32cf7de58e4fa0
Atil Samancioglu <samancioglu.atil@gmail.com>
2024-10-28 17:31:04 +0300

    Initial Commit

---
+++ SwiftTestExamples/SwiftTestExamplesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftTestExamplesApp.swift
+//  SwiftTestExamples
+//
+//  Created by Atil Samancioglu on 28.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftTestExamplesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
